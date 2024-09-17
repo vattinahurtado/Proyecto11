e5b904d82f41b8917ad668fa24bbe6b3f36f4437
vattinahurtado <valentinahurtado072@gmail.com>
2024-09-17 14:13:13 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -33,11 +33,11 @@
     <!-- About us section -->
     <section class="about-us">
         <h2>Quienes somos</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+        <p>Somos una pagina web encargada de la matriculación e inscripción de estudiantes a la institucion ICIT,nuestro principal objetivo es la agilización de procesos que anteriormente fueron mencionados,de una manera mas rapida y efectiva.</p>
         <h2>Misión</h2>
-        <p>Somos InscripMate Solutions una empresa que busca fomentar la eficiencia y eficacia de las institución educativa ICIT, en el área de inscripciones y matriculas, nuestro objetivo es facilitarle a los padres de familia de los estudiantes en este proceso para así realizar la carga de sus documentos de manera mas efectiva por medio de nuestra pagina web y agilizando el protocolo de firmas.</p>
+        <p>Somos InscripMate Solutions una empresa que busca fomentar la eficiencia y eficacia de la institución educativa ICIT, en el área de inscripciones y matriculas, nuestro objetivo es facilitar a los padres de familia de los estudiantes en este proceso para así realizar la carga de sus documentos de manera mas efectiva por medio de nuestra pagina web y agilizando el protocolo de firmas.</p>
         <h2>Visión</h2>
-        <p>Para el año 2026 esperamos ser una pagina reconocida a nivel nacional para que de esta manera sigamos mejorando los servicios de nuestra pagina y que de esta manera se logre implementar a mas instituciones educativas de nuestro país y que todos los padres de familia utilicen nuestra pagina para sus documentos y la firma rápida de ellos.</p>
+        <p>Para el año 2026 esperamos ser una pagina reconocida a nivel nacional para que de esta manera sigamos mejorando los servicios de nuestra pagina web y asi mismo se logre implementar a mas instituciones educativas de nuestro país y que todos los padres de familia utilicen nuestra pagina para sus documentos y la firma rápida de ellos.</p>
     </section>
 
     <!-- Photo collage section -->
